d4655fd6a662ba3ebb2a2c89e5a24d387e11e76f
AshuJavaDev <nishukr5454@gmail.com>
2026-05-16 17:30:45 +0530

    Problem 29 - Maximum Sum Subarray of Size K

---
+++ src/strings/MaxSumSubarrayK.java
@@ -0,0 +1,34 @@
+package strings;
+
+import java.util.HashSet;
+
+public class MaxSumSubarrayK {
+    public static void main(String[] args)  {
+
+        int[] arr = {1, 4, 2, 10, 23, 3, 1, 0, 20};
+        int k =4;
+        int left = 0;
+        int sum = 0;
+        int maxcount = 0;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int right = k-1; right <arr.length; right++)    {
+
+                if(left==0)  {
+                for(int i = 0; i<= k-1; i++) {
+                sum += arr[i];
+            }
+                left++;
+
+                } else  {
+                    sum = sum - arr[left-1] + arr[right];
+                    left++;
+                }
+                    maxcount = Math.max(maxcount,sum);
+        }
+                    System.out.println(maxcount);
+    }
+}
+
+
