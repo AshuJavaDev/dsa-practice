fae069fee0234582643484d9eeed837af70ae85d
AshuJavaDev <nishukr5454@gmail.com>
2026-06-06 13:11:29 +0530

    Problem 47 - Print 1 to N using Recursion

---
+++ src/recursion/RecursionBasics.java
@@ -0,0 +1,20 @@
+package recursion;
+
+class RecursionBasics {
+    static void print(int n) {
+        if (n != 0) {
+            print(n - 1);
+            System.out.print(n + " ");
+
+        }
+    }
+        public static void main(String[] args) {
+            print(5);
+
+    }
+}
+
+
+
+
+
