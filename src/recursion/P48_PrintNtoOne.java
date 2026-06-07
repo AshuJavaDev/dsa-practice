c7c2b2c44b75589c28141da7c4248e622599fd6e
AshuJavaDev <nishukr5454@gmail.com>
2026-06-07 13:18:15 +0530

    Problem 48 - Print N to 1 using Recursion

---
+++ src/recursion/P48_PrintNtoOne.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class P48_PrintNtoOne {
+        static void print(int n)    {
+        if(n!=0)    {
+            System.out.print(n + " ");
+                print(n-1);
+            }
+        }
+        public static void main(String[] args)  {
+            print(5);
+        }
+    }
+
+
+
+
+
+
+
+
