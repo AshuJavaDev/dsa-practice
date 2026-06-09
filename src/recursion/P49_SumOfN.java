bd73849e9fc8e17773688a1c69967cbf2fe91ebf
AshuJavaDev <nishukr5454@gmail.com>
2026-06-09 12:34:22 +0530

    Problem 49 - Sum of N Numbers using Recursion

---
+++ src/recursion/P49_SumOfN.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class P49_SumOfN {
+        static int sum(int n)   {
+            if( n == 0 ) return 0;
+            return n + sum(n-1);
+        }
+
+
+        public static void main(String[] args)  {
+            int result = sum(5);
+            System.out.println(result);
+        }
+    }
+
+
+
+
