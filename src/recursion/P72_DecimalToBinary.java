ed79a502a1e7cee4c9df6a8cffa694550a88b7a3
AshuJavaDev <nishukr5454@gmail.com>
2026-07-27 17:06:37 +0530

    P72: Decimal to binary using recursion

---
+++ src/recursion/P72_DecimalToBinary.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class P72_DecimalToBinary {
+    static void decimalToBinary(int n)   {
+        if(n == 0)  return;
+
+        decimalToBinary(n/2);
+        System.out.print(n%2);
+
+    }
+
+    public static void main(String[] args)  {
+        int n = 13;
+        decimalToBinary(n);
+    }
+}
